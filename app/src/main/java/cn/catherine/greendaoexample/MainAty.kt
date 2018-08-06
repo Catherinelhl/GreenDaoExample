cab997b5a224eb3b9dfb45182c29310dd71e7259
Catherine.Brain William <15520054289@163.com>
2018-08-07 00:29:51 +0800

    modify

--- app/src/main/java/cn/catherine/greendaoexample/MainAty.kt
+++ app/src/main/java/cn/catherine/greendaoexample/MainAty.kt
@@ -6,6 +6,8 @@ import android.os.Bundle
 import cn.catherine.data.*
 import cn.catherine.data.DaoMaster.DevOpenHelper
 import kotlinx.android.synthetic.main.aty_main.*
+import org.greenrobot.greendao.query.WhereCondition
+
 
 class MainAty : AppCompatActivity() {
     private lateinit var sqLiteDatabase: SQLiteDatabase
@@ -19,8 +21,9 @@ class MainAty : AppCompatActivity() {
         setContentView(R.layout.aty_main)
         openDB()
         add.setOnClickListener {
-//            add()
-            getAll()
+            //            add()
+//            queryAll()
+            diyQuery()
         }
     }
 
@@ -33,15 +36,15 @@ class MainAty : AppCompatActivity() {
     }
 
     fun add() {
-        val name = name.text.toString()
-        val age = age.text.toString().toInt()
+//        val name = name.text.toString()
+//        val age = age.text.toString().toInt()
         var son = Son()
-        son.name = name
-        son.age = age
+        son.name = "kate"
+        son.age = 38
 
         var father = Father()
-        father.hobby = "下棋"
-        father.tall = 165
+        father.hobby = "dancing"
+        father.tall = 168
 
         var fatherId = fatherDao.insert(father)
         son.fatherId = fatherId
@@ -50,19 +53,31 @@ class MainAty : AppCompatActivity() {
 
     }
 
-    fun delete() {}
-    fun update() {}
-    private fun getAll() {
-        var fathers=fatherDao.queryBuilder().list()
-        var sons=sonDao.queryBuilder().list()
-        for (i in fathers.indices){
+    fun delete() {
+        sonDao.deleteAll()
+        fatherDao.deleteAll()
+    }
+
+    private fun queryAll() {
+        var fathers = fatherDao.queryBuilder().list()
+        var sons = sonDao.queryBuilder().list()
+        for (i in fathers.indices) {
             println("=====${fathers[i]}")
         }
-        for (i in sons.indices){
+        for (i in sons.indices) {
             println("=====${sons[i]}")
         }
 
     }
-    fun save() {}
+
+    private fun diyQuery() {//原始查询
+        val sons = sonDao.queryBuilder().where(
+                WhereCondition.StringCondition("FATHER_ID IN " + "(SELECT _ID FROM FATHER WHERE TALL = 168)")
+        ).build().list()
+
+        for (i in sons.indices) {
+            println("====${sons[i]}")
+        }
+    }
 
 }
